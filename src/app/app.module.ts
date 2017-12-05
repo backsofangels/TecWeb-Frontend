9479796dd0b29f8a17b4ed80f327673efd38df25
Fabrizio <fabrizio.musella@gmail.com>
2017-12-05 22:00:57 +0100

    First header version, first routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      SignupComponent,
+      LoginComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
