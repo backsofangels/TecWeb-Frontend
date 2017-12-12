e46f0b598b2400901e10e0a68be80a0895d5ad72
Cristina Salerno <cristina.salerno94@gmail.com>
2017-12-12 23:04:43 +0100

    Altri fixing profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  regioni = ['Really Smart', 'Super Flexible',
-  'Super Hot', 'Weather Changer'];
+  regioni = ['Milano', 'Napoli', 'Roma'];
 
   constructor() { }
 
