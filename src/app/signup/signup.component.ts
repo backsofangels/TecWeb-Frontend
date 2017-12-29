3a2fb527c6abcccffb4278d3e593d7e4da57f9b7
Fabrizio <fabrizio.musella@gmail.com>
2017-12-29 18:24:10 +0100

    Created HTTP request for signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+    private router: Router;
 
-  ngOnInit() {
+    constructor(private http: HttpClient) {
+    }
+
+    // Bisogna inserire all'interno dei vari set i valori presi dal form
+    onSubmit() {
+        const params = new HttpParams()
+            .set('firstName', 'inserisci valore del form')
+            .set('lastName', 'inserisci valore del form')
+            .set('email', 'inserisci valore del form')
+            .set('pwd', 'inserisci valore del form')
+            .set('favoriteDrill', '-1');
+        this.http.post('signup', params).subscribe(() => {
+                this.router.navigateByUrl('/login');
+            }
+        );
   }
 
 }
