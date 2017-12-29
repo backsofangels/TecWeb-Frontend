83587e682cfd226fa03d98cf860c06683ea33bc6
Fabrizio <fabrizio.musella@gmail.com>
2017-12-29 18:44:33 +0100

    Cleanup of mocks files (We have the server up and running thanks to Sasy <3)

--- src/app/mocks/userjwt.mocks.ts
+++
@@ -1,5 +0,0 @@
-export class UserJWT {
-    userJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZGVudGlmaWVyIjo3LCJmaXJzdE5hbWUiOiJTYWx2YXRvcmUiLCJsYXN0TmFtZSI6IkdJR0lHSSIsImZhdm9yaXRlRHJpbGwiOjEsImlzcyI6InBvbGx1dGVjaC5jb20iLCJleHAiOjE1MTI3MzYxMDgsImVtYWlsIjoiYmFja3NvZmFuZ2Vsc0BnbWFpbC5jb20ifQ.sRN4dlQKpaH7OmRvJeDtkU0gXf_3iu-zJU9FgX3Jq58";
-    jwtIssuer = "pollutech.com";
-    jwtSecret = "secret";
-}
