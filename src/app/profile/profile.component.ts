c1856f8fca270cb9a8934db33bd7c5351a2a6db1
Fabrizio <fabrizio.musella@gmail.com>
2018-01-02 16:21:15 +0100

    User update works

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService} from "../AuthService";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+    private auth: AuthService = new AuthService(this.http);
+    private updateOk: boolean = false;
+    private errorUpdate: boolean = false;
 
-  constructor() {
+    constructor(private http: HttpClient) {
   }
 
   email: string;
@@ -16,7 +21,27 @@ export class ProfileComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     if (form.valid) {
-      console.log(form.value);
+        let drillID = JSON.parse(localStorage.getItem('user')).favoriteDrill;
+        let firstName = JSON.parse(localStorage.getItem('user')).firstName;
+        let lastName = JSON.parse(localStorage.getItem('user')).lastName;
+        console.log(form.value);
+        const body = JSON.stringify({
+            firstName: firstName, lastName: lastName, email: form.value.email,
+            pwd: form.value.pwd, favoriteDrill: drillID
+        });
+        this.auth.update(body)
+            .subscribe(
+                () => {
+                    console.log('Update successful');
+                    this.updateOk = true;
+                },
+                (error) => {
+                    this.errorUpdate = true;
+                    console.log('Update failed');
+                    console.log(error);
+//                    this.errorLogin = true;
+                }
+            );
       // ...our form is valid, we can submit the data
     }
   }
